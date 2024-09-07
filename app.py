5a100018f2a8d1518c35440f5a6cb71f56464dd1
macaabarzua <macaabarzua@gmail.com>
2024-09-07 11:09:52 -0400

    testeo clases y objetos

--- app.py
+++ app.py
@@ -3,22 +3,28 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-import data_wrangling
+from data_wrangling import Limpiador
 
 def main():
-    datos = pd.read_csv('./megaline_users.csv')
+    datos_raw = pd.read_csv('./megaline_users.csv')
     
-    datos = data_wrangling.clean(datos)
+    limpiador = Limpiador('limpiador de películas', datos_raw)
+
+    print(datos_raw.isna().sum())
+
+    datos_clean = limpiador.clean()
+
+    print(datos_clean.isna().sum())
 
     #print(datos.info())
 
-    boton_histograma = st.button('Click aquí para dibujar el histograma')
+    #boton_histograma = st.button('Click aquí para dibujar el histograma')
 
-    if boton_histograma:
-        fig = px.histogram(datos, 'reg_date')
-        fig.show()
+    #if boton_histograma:
+        #fig = px.histogram(datos, 'reg_date')
+        #fig.show()
 
-        st.plotly_chart(fig)
+        #st.plotly_chart(fig)
 
 if __name__ == '__main__':
     main()
--- data_wrangling.py
+++ data_wrangling.py
@@ -3,6 +3,18 @@ import pandas as pd
 def clean(datos):
     return datos.dropna()
 
+class Limpiador():
+
+    nombre = 'sin nombre'
+    datos = None
+
+    def __init__(self, nombre_personalizado, datos_personalizados):
+        self.nombre = nombre_personalizado
+        self.datos = datos_personalizados
+
+    def clean(self):
+        return self.datos.dropna()
+
 if __name__ == '__main__':
     print('Esta es una librería de limpieza de datos')
     
